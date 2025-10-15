341fe134ae77eb6fa26af556500fa8825a201189
Ite-O <61171701+Ite-O@users.noreply.github.com>
2025-10-15 19:08:23 +1100

    Comments for Indexed_Heap and moved HeapItem to separate file

--- IndexedHeap_Stub.py
+++ IndexedHeap_Stub.py
@@ -1,13 +1,13 @@
-from iheap import HeapItem, miniheap, maxiheap
+from Indexed_Heap import HeapItem, MinHeap, MaxHeap
 
 class _HeapStubMixin:
     def __init__(self, heap, item_to_index):
         self.heap = [HeapItem(item) for item in heap]
         self.item_to_index = item_to_index
 
-class max_iheap_stub(_HeapStubMixin, maxiheap):
+class max_heap_stub(_HeapStubMixin, MaxHeap):
     pass
 
 
-class min_iheap_stub(_HeapStubMixin, miniheap):
+class min_heap_stub(_HeapStubMixin, MinHeap):
     pass
--- tests_IndexedHeap.py
+++ tests_IndexedHeap.py
@@ -1,5 +1,5 @@
 import unittest
-import iheap
+import Indexed_Heap
 import math
 import IndexedHeap_Stub
 
@@ -9,70 +9,70 @@ class Test_IndexedHeap(unittest.TestCase):
         arr_2 = ["ababacc", "brown", "yellow", "abc", "aaaaaaa"]
         arr_3 = [-1000,-1.5,500, 300, math.pi]
 
-        max_heaps =[iheap.maxiheap.heapify(arr_1),
-                    iheap.maxiheap.heapify(arr_2),
-                    iheap.maxiheap.heapify(arr_3)
+        max_heaps =[Indexed_Heap.MaxHeap(arr_1),
+                    Indexed_Heap.MaxHeap(arr_2),
+                    Indexed_Heap.MaxHeap(arr_3)
                     ]
-        min_heaps = [iheap.miniheap.heapify(arr_1),
-                     iheap.miniheap.heapify(arr_2),
-                     iheap.miniheap.heapify(arr_3)
-                     ]
+        min_heaps = [Indexed_Heap.MinHeap(arr_1),
+                    Indexed_Heap.MinHeap(arr_2),
+                    Indexed_Heap.MinHeap(arr_3)
+                    ]
         
         min_heaps_stubs = [
-                    IndexedHeap_Stub.min_iheap_stub(
+                    IndexedHeap_Stub.min_heap_stub(
                         heap = [-1000,-1.5,500, 300, math.pi], 
-                        item_to_index = {iheap.HeapItem(-1000): 0, 
-                                        iheap.HeapItem(-1.5): 1,
-                                        iheap.HeapItem(500): 2,
-                                        iheap.HeapItem(300): 3,
-                                        iheap.HeapItem(math.pi): 4
+                        item_to_index = {Indexed_Heap.HeapItem(-1000): 0, 
+                                        Indexed_Heap.HeapItem(-1.5): 1,
+                                        Indexed_Heap.HeapItem(500): 2,
+                                        Indexed_Heap.HeapItem(300): 3,
+                                        Indexed_Heap.HeapItem(math.pi): 4
                                         }             
                     ),
-                    IndexedHeap_Stub.min_iheap_stub(
+                    IndexedHeap_Stub.min_heap_stub(
                         heap = ["aaaaaaa", "ababacc", "yellow", "abc", "brown"], 
-                        item_to_index = {iheap.HeapItem("aaaaaaa"): 0, 
-                                        iheap.HeapItem("ababacc"): 1,
-                                        iheap.HeapItem("yellow"): 2,
-                                        iheap.HeapItem("abc"): 3,
-                                        iheap.HeapItem("brown"): 4
+                        item_to_index = {Indexed_Heap.HeapItem("aaaaaaa"): 0, 
+                                        Indexed_Heap.HeapItem("ababacc"): 1,
+                                        Indexed_Heap.HeapItem("yellow"): 2,
+                                        Indexed_Heap.HeapItem("abc"): 3,
+                                        Indexed_Heap.HeapItem("brown"): 4
                                         } 
                     ),
-                    IndexedHeap_Stub.min_iheap_stub(
+                    IndexedHeap_Stub.min_heap_stub(
                         heap = [-1000,-1.5,500, 300, math.pi], 
-                        item_to_index = {iheap.HeapItem(-1000): 0, 
-                                        iheap.HeapItem(-1.5): 1,
-                                        iheap.HeapItem(500): 2,
-                                        iheap.HeapItem(300): 3,
-                                        iheap.HeapItem(math.pi): 4
+                        item_to_index = {Indexed_Heap.HeapItem(-1000): 0, 
+                                        Indexed_Heap.HeapItem(-1.5): 1,
+                                        Indexed_Heap.HeapItem(500): 2,
+                                        Indexed_Heap.HeapItem(300): 3,
+                                        Indexed_Heap.HeapItem(math.pi): 4
                                         }             
                     )                 
         ]
         max_heaps_stubs = [
-                    IndexedHeap_Stub.max_iheap_stub(
+                    IndexedHeap_Stub.max_heap_stub(
                         heap = [500, 300, -1000, -1.5, math.pi], 
-                        item_to_index = {iheap.HeapItem(500): 0, 
-                                        iheap.HeapItem(300): 1,
-                                        iheap.HeapItem(-1000): 2,
-                                        iheap.HeapItem(-1.5): 3,
-                                        iheap.HeapItem(math.pi): 4
+                        item_to_index = {Indexed_Heap.HeapItem(500): 0, 
+                                        Indexed_Heap.HeapItem(300): 1,
+                                        Indexed_Heap.HeapItem(-1000): 2,
+                                        Indexed_Heap.HeapItem(-1.5): 3,
+                                        Indexed_Heap.HeapItem(math.pi): 4
                                         }             
                     ),
-                    IndexedHeap_Stub.max_iheap_stub(
+                    IndexedHeap_Stub.max_heap_stub(
                         heap = ["yellow", "brown", "ababacc", "abc", "aaaaaaa"], 
-                        item_to_index = {iheap.HeapItem("yellow"): 0, 
-                                        iheap.HeapItem("brown"): 1,
-                                        iheap.HeapItem("ababacc"): 2,
-                                        iheap.HeapItem("abc"): 3,
-                                        iheap.HeapItem("aaaaaaa"): 4
+                        item_to_index = {Indexed_Heap.HeapItem("yellow"): 0, 
+                                        Indexed_Heap.HeapItem("brown"): 1,
+                                        Indexed_Heap.HeapItem("ababacc"): 2,
+                                        Indexed_Heap.HeapItem("abc"): 3,
+                                        Indexed_Heap.HeapItem("aaaaaaa"): 4
                                         } 
                     ),
-                    IndexedHeap_Stub.max_iheap_stub(
+                    IndexedHeap_Stub.max_heap_stub(
                         heap = [500,300,-1000, -1.5, math.pi] , 
-                        item_to_index = {iheap.HeapItem(500): 0, 
-                                        iheap.HeapItem(300): 1,
-                                        iheap.HeapItem(-1000): 2,
-                                        iheap.HeapItem(-1.5): 3,
-                                        iheap.HeapItem(math.pi): 4
+                        item_to_index = {Indexed_Heap.HeapItem(500): 0, 
+                                        Indexed_Heap.HeapItem(300): 1,
+                                        Indexed_Heap.HeapItem(-1000): 2,
+                                        Indexed_Heap.HeapItem(-1.5): 3,
+                                        Indexed_Heap.HeapItem(math.pi): 4
                                         } 
                     )
                                      
@@ -85,7 +85,7 @@ class Test_IndexedHeap(unittest.TestCase):
 
     def test_insert_peak_pop_with_duplicates(self):
         def test_min():
-            min_heap = iheap.miniheap()
+            min_heap = Indexed_Heap.MinHeap()
             min_heap.insert(10)
             self.assertEqual(min_heap.peek(), 10)
             min_heap.insert(5)
@@ -107,7 +107,7 @@ class Test_IndexedHeap(unittest.TestCase):
                 min_heap.pop()
         
         def test_max():
-            max_heap = iheap.maxiheap()
+            max_heap = Indexed_Heap.MaxHeap()
             max_heap.insert(10)
             self.assertEqual(max_heap.peek(), 10)
             max_heap.insert(15)
@@ -133,7 +133,7 @@ class Test_IndexedHeap(unittest.TestCase):
 
     def test_insert_remove_count(self):
         def test_min():
-            min_heap = iheap.miniheap()
+            min_heap = Indexed_Heap.MinHeap()
             for _ in range(5):
                 min_heap.insert(5)
             for i in range(10, 0, -1):
@@ -146,9 +146,15 @@ class Test_IndexedHeap(unittest.TestCase):
             min_heap.remove(5, 5)
             self.assertEqual(min_heap.count(5), 0)
             self.assertEqual(len(min_heap), 9)
+            min_heap.remove(5, strict=False)
+            self.assertEqual(len(min_heap), 9)
+            min_heap.remove(1, 100, strict=False)
+            self.assertEqual(len(min_heap), 8)
+            ordered_list = list(min_heap)
+            self.assertEqual(len(ordered_list), 8)
 
         def test_max():
-            max_heap = iheap.maxiheap()
+            max_heap = Indexed_Heap.MaxHeap()
             for _ in range(5):
                 max_heap.insert(5)
             for i in range(10, 0, -1):
@@ -161,6 +167,17 @@ class Test_IndexedHeap(unittest.TestCase):
             max_heap.remove(5, 5)
             self.assertEqual(max_heap.count(5), 0)
             self.assertEqual(len(max_heap), 9)
+            max_heap.remove(5, strict=False)
+            self.assertEqual(len(max_heap), 9)
+            max_heap.remove(1, 100, strict=False)
+            self.assertEqual(len(max_heap), 8)
+            max_heap.insert(10, 3)
+            ordered_list = list(max_heap)
+            self.assertEqual(len(ordered_list), 11)
+            
+            a = Indexed_Heap.MaxHeap()
+            b = Indexed_Heap.MaxHeap()
+            print(a == b)
 
         test_min()
         test_max()
